561d4031b86b07ca933663269517f01d4a36ec5e
IsanovTim <tim_h@mail.ru>
2022-03-29 21:50:13 +0300

    qq

--- src/main/java/com/tim_h/Main.java
+++ src/main/java/com/tim_h/Main.java
@@ -1,11 +1,20 @@
 package com.tim_h;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.log4j.PropertyConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Slf4j
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("test");
-        System.out.println("thth");
-        System.out.println("first");
-        System.out.println("kkk");
+        PropertyConfigurator.configure("C:\\Users\\tim_h\\IdeaProjects\\test-maven-project\\log4j.properties");
+        log.info("hello world!");
     }
+
 }
